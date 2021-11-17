773e4ee84199edc2fa04f651e930337a2280e8e1
Nick Fulton <nickfulton@Nick-MBP.local>
2021-11-17 14:25:48 -0700

    Initialize

---
+++ src/test/java/com/cse412team18/pos/PosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cse412team18.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
