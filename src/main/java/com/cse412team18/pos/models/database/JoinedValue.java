8caaf67ce80a97bf16c6d485f6b4f86054d2ace8
Nick Fulton <nkfulton@asu.edu>
2021-11-17 16:34:17 -0700

    Add database helpers

---
+++ src/main/java/com/cse412team18/pos/models/database/JoinedValue.java
@@ -0,0 +1,6 @@
+package com.cse412team18.pos.models.database;
+
+public class JoinedValue<T> {
+    public boolean loaded;
+    public T value;
+}
---
+++ src/main/java/com/cse412team18/pos/models/database/ProductOnReceipt.java
@@ -0,0 +1,8 @@
+package com.cse412team18.pos.models.database;
+
+import com.cse412team18.pos.models.Product;
+
+public class ProductOnReceipt {
+    public Product product;
+    public int count;
+}
