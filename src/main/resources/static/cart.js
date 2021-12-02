af035c2e055591794a2cbc4d6a3a983a0cfb2c52
Nick Fulton <nkfulton@asu.edu>
2021-12-02 00:04:13 -0700

    Add product list

---
+++ src/main/resources/static/cart.js
@@ -0,0 +1,26 @@
+$(function () {
+    localStorage.clear();
+
+    $.get('/product/all', (data) => {
+        var ul = $('#itemlist');
+        data.forEach(item => {
+            var li = $('<li/>')
+                .text(item.displayName + " ")
+                .appendTo(ul);
+            
+            $('<a/>')
+                .text("Add")
+                .attr("href", "#")
+                .click(item.id, click)
+                .appendTo(li);
+        });
+    });
+});
+
+function click(event) {
+    var current = localStorage.getItem(event.data.toString());
+    if (current)
+        localStorage.setItem(event.data.toString(), Number.parseInt(current) + 1);
+    else
+        localStorage.setItem(event.data.toString(), 1);
+}
