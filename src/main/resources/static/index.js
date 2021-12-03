50f7b8711280d98efcc429bb2a1fdf030574a297
Nick Fulton <nkfulton@asu.edu>
2021-12-02 17:06:25 -0700

    Integrate member retrieval

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,13 +1,23 @@
 $(function () {
     localStorage.clear();
+    $("#submit").click(submit);
+    $("#skip").click(skip);
 });
 
-function member(input){
-    var memberInfo = databaseService.getMember(input);
-    if(memberInfo)
-        localStorage.setItem('member', true);
-    else
-        localStorage.setItem('member', false);
-    
+function submit() {
+    var phoneNumber = $("#phoneNumber").val();
+    $.get(`/member?phoneNumber=${encodeURIComponent(phoneNumber)}`, data => {
+        if (!data) {
+            alert('User not found');
+        }
+        else {
+            localStorage.setItem('member', JSON.stringify(data));
+            window.location.href = "products.html";
+        }
+    });
 }
 
+function skip() {
+    localStorage.setItem('member', 'null');
+    window.location.href = "products.html";
+}
