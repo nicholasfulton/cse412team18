5f46732ec050fc5c9a81f643546a35dbfb59e6f4
Nick Fulton <nkfulton@asu.edu>
2021-12-02 18:55:08 -0700

    Add receipt controller

---
+++ src/main/java/com/cse412team18/pos/controllers/ReceiptController.java
@@ -0,0 +1,28 @@
+package com.cse412team18.pos.controllers;
+
+import com.cse412team18.pos.models.ReceiptModel;
+import com.cse412team18.pos.services.IDatabaseService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/receipt")
+public class ReceiptController {
+    @Autowired
+    private IDatabaseService databaseService;
+
+    @PostMapping
+    public void createReceipt(@RequestBody() ReceiptModel receipt) {
+        databaseService.createReceipt(receipt);
+    }
+
+    @GetMapping("/next")
+    public int nextId() {
+        return databaseService.nextReceiptId();
+    }
+}
